4797591695f7a999d8dcaad9f0f4751a295f2c67
Timur Nazarov <nazarovtr@gmail.com>
2018-10-30 11:07:55 +0100

    Examples

--- src/main/java/se/ox/timur/concurrency/executor/ThreadPoolExecutorDemo.java
+++ src/main/java/se/ox/timur/concurrency/executor/ThreadPoolExecutorDemo.java
@@ -1,4 +1,13 @@
 package se.ox.timur.concurrency.executor;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ThreadPoolExecutorDemo {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        executorService.submit(() -> doStuff());
+    }
+
+    private static void doStuff() {}
 }
